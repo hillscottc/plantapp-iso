19ec86a62d7eda470f0d8cd60aeb85c0154cfc5f
Scott Hill <scott289@gmail.com>
2016-11-02 19:56:52 -0700

    api routes with Plants data works
    db names

---
+++ src/api_routes.js
@@ -0,0 +1,59 @@
+/**
+ * Server routes at /api/
+ */
+import {Plant, setPlantsQuery} from './data/models/Plant';
+import express from 'express';
+
+const router = express.Router();
+
+// GET plants all
+router.get('/plants/', (req, res) => {
+
+  const {family, common, symbol, sci, limit, offset} = req.query;
+
+  Plant.forge()
+      .query((qb) => {
+        //qb is knex query builder
+        setPlantsQuery(qb, {family, common, symbol, sci});
+      })
+      .fetchPage({limit, offset})
+      .then((plants) => {
+        return res.json({
+          data: plants.toJSON(),
+          pagination: plants.pagination
+        })
+      })
+      .catch((err) => {
+        return res.status(500).json({error: true, data: {message: err.message}});
+      });
+});
+
+
+// POST QUERY
+// Accepts post of search args to return plant records.
+router.post('/plants/', (req, res) => {
+
+  const {family, common, symbol, sci, limit, offset} = req.body;
+
+  debug("Handling POST:", req.body);
+
+  Plant.forge()
+      .query((qb) => {
+        //qb is knex query builder
+        setPlantsQuery(qb, {family, common, symbol, sci});
+      })
+      .fetchPage({limit, offset})
+      .then((plants) => {
+        return res.json({
+          data: plants.toJSON(),
+          pagination: plants.pagination
+        })
+      })
+      .catch((err) => {
+        return res.status(500).json({error: true, data: {message: err.message}});
+      });
+
+});
+
+
+module.exports = router;
--- src/config.js
+++ src/config.js
@@ -9,10 +9,12 @@
 
 /* eslint-disable max-len */
 
+
 export const port = process.env.PORT || 3000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
-export const databaseUrl = process.env.DATABASE_URL || 'sqlite:database.sqlite';
+export const databaseUrl =  process.env.DATABASE_URL || 'postgres:///triv_db';
+export const sqlLiteUrl = 'sqlite:database.sqlite';
 
 export const analytics = {
 
