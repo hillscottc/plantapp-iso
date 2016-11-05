4028419074083c9bda6344a67f169076d9223465
Scott Hill <scott289@gmail.com>
2016-11-05 12:29:51 -0700

    plants gql works

--- src/data/bookshelf-db.js
+++ src/data/bookshelf-db.js
@@ -1,16 +1,8 @@
-// const config = require('../config');
-//
-// const knex = require('knex')({client: 'pg', connection: config.databaseUrl });
-//
-// module.exports = require('bookshelf')(knex);
-
-
 import knex from 'knex';
 import bookshelf from 'bookshelf';
 
 // Can't use config values here.
 // import config  from '../config';
-
 
 const Bookshelf = bookshelf(knex(
   {client: 'pg', connection: 'postgres:///hills120' }
--- src/data/models/Plant.js
+++ src/data/models/Plant.js
@@ -1,13 +1,21 @@
 import Bookshelf from '../bookshelf-db';
 
+
+// I probably dont need fir gql queries
 Bookshelf.plugin('pagination');
+
+
+class Plant extends Bookshelf.Model {
+  get tableName() { return 'plantapp.plant'; }
+}
+
 
 /**
  * Sets a knex query builder object for given search params.
  * @param qb a knex query builder
  * @param queryArgs for plants
  */
-function setPlantsQuery(qb, queryArgs) {
+Plant.setPlantsQuery = (qb, queryArgs) => {
   let {family, common, symbol, sci} = queryArgs;
 
   family = family ? "%" + family + "%" : "";
@@ -19,11 +27,8 @@ function setPlantsQuery(qb, queryArgs) {
   if (common) qb.where('common_name', 'like', common);
   if (family) qb.where('family', 'like', family);
   if (sci) qb.where('sci_name', 'like', sci);
-}
+};
 
-class Plant extends Bookshelf.Model {
-  get tableName() { return 'plantapp.plant'; }
-}
 
 export default Plant;
 
