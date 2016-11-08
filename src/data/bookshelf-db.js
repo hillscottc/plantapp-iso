1868e782b0772517923c35d0218097bd48a7a508
Scott Hill <scott289@gmail.com>
2016-11-08 10:02:44 -0800

    db url

--- src/data/bookshelf-db.js
+++ src/data/bookshelf-db.js
@@ -5,7 +5,8 @@ import bookshelf from 'bookshelf';
 // import config  from '../config';
 
 const Bookshelf = bookshelf(knex(
-  {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///hills120' }
+  // {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///hills120' }
+  {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///triv_db' }
 ));
 
 export default Bookshelf;
